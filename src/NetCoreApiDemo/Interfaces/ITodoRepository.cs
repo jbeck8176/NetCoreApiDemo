3e2f761f590ae72a67fd14d3657874824f4733d0
Jeff Beck <jeff@squints.io>
2016-09-22 18:56:11 -0400

    Demo site checked in

---
+++ src/NetCoreApiDemo/Interfaces/ITodoRepository.cs
@@ -0,0 +1,15 @@
+﻿using NetCoreApiDemo.Models;
+using System.Collections.Generic;
+
+
+namespace NetCoreApiDemo.Interfaces
+{
+  public interface ITodoRepository
+  {
+    void Add(TodoItem item);
+    IEnumerable<TodoItem> GetAll();
+    TodoItem Find(string key);
+    TodoItem Remove(string key);
+    void Update(TodoItem item);
+  }
+}
