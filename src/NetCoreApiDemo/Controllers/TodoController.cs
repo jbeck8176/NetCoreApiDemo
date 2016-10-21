7bacc37e89dee4dc77590fb6c613af4b954d5d9e
jbeck <jbeck8176@gmail.com>
2016-10-21 14:36:14 -0400

    disabled cors and made tweaks to the todo controller

--- src/NetCoreApiDemo/Controllers/TodoController.cs
+++ src/NetCoreApiDemo/Controllers/TodoController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using NetCoreApiDemo.Models;
 using NetCoreApiDemo.Interfaces;
@@ -10,92 +7,92 @@ using NetCoreApiDemo.Interfaces;
 
 namespace NetCoreApiDemo.Controllers
 {
-  [Route("api/[controller]")]
-  public class TodoController : Controller
-  {
-    private ITodoRepository TodoRepo { get; set; }
-    public TodoController(ITodoRepository todoRepo)
+    [Route("api/[controller]")]
+    public class TodoController : Controller
     {
-      TodoRepo = todoRepo;
+        private ITodoRepository TodoRepo { get; set; }
+        public TodoController(ITodoRepository todoRepo)
+        {
+            TodoRepo = todoRepo;
+        }
+
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return TodoRepo.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(string id)
+        {
+            var item = TodoRepo.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            TodoRepo.Add(item);
+            return GetById(item.Key);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Key != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = TodoRepo.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            TodoRepo.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Update([FromBody] TodoItem item, string id)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+
+            var todo = TodoRepo.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            item.Key = todo.Key;
+
+            TodoRepo.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            var todo = TodoRepo.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            TodoRepo.Remove(id);
+            return new NoContentResult();
+        }
     }
-
-    [HttpGet]
-    public IEnumerable<TodoItem> GetAll()
-    {
-      return TodoRepo.GetAll();
-    }
-
-    [HttpGet("{id}", Name = "GetTodo")]
-    public IActionResult GetById(string id)
-    {
-      var item = TodoRepo.Find(id);
-      if (item == null)
-      {
-        return NotFound();
-      }
-      return new ObjectResult(item);
-    }
-
-    [HttpPost]
-    public IActionResult Create([FromBody] TodoItem item)
-    {
-      if (item == null)
-      {
-        return BadRequest();
-      }
-      TodoRepo.Add(item);
-      return CreatedAtRoute("GetTodo", new { id = item.Key }, item);
-    }
-
-    [HttpPut("{id}")]
-    public IActionResult Update(string id, [FromBody] TodoItem item)
-    {
-      if (item == null || item.Key != id)
-      {
-        return BadRequest();
-      }
-
-      var todo = TodoRepo.Find(id);
-      if (todo == null)
-      {
-        return NotFound();
-      }
-
-      TodoRepo.Update(item);
-      return new NoContentResult();
-    }
-
-    [HttpPatch("{id}")]
-    public IActionResult Update([FromBody] TodoItem item, string id)
-    {
-      if (item == null)
-      {
-        return BadRequest();
-      }
-
-      var todo = TodoRepo.Find(id);
-      if (todo == null)
-      {
-        return NotFound();
-      }
-
-      item.Key = todo.Key;
-
-      TodoRepo.Update(item);
-      return new NoContentResult();
-    }
-
-    [HttpDelete("{id}")]
-    public IActionResult Delete(string id)
-    {
-      var todo = TodoRepo.Find(id);
-      if (todo == null)
-      {
-        return NotFound();
-      }
-
-      TodoRepo.Remove(id);
-      return new NoContentResult();
-    }
-  }
 }
--- src/NetCoreApiDemo/Startup.cs
+++ src/NetCoreApiDemo/Startup.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,37 +7,48 @@ using Microsoft.Extensions.Logging;
 
 namespace NetCoreApiDemo
 {
-  public class Startup
-  {
-    public Startup(IHostingEnvironment env)
+    public class Startup
     {
-      var builder = new ConfigurationBuilder()
-          .SetBasePath(env.ContentRootPath)
-          .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-          .AddEnvironmentVariables();
-      Configuration = builder.Build();
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+
+            var corsBuilder = new CorsPolicyBuilder();
+            corsBuilder.AllowAnyHeader();
+            corsBuilder.AllowAnyMethod();
+            corsBuilder.AllowAnyOrigin();
+            corsBuilder.AllowCredentials();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", corsBuilder.Build());
+            });
+
+            //Add the dependancy injection...
+            services.AddSingleton<Interfaces.ITodoRepository, Repositories.TodoRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+            app.UseCors("AllowAll");
+            app.UseMvc();
+        }
     }
-
-    public IConfigurationRoot Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      // Add framework services.
-      services.AddMvc();
-
-      //Add the dependancy injection...
-      services.AddSingleton<Interfaces.ITodoRepository, Repositories.TodoRepository>();
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-    {
-      loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-      loggerFactory.AddDebug();
-
-      app.UseMvc();
-    }
-  }
 }
